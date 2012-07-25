1564380d99741bcf1405796f96e10333cf894d60
Claudel Sylvain <claudel.sylvain@gmail.com>
2012-07-25 22:32:39 +0200

    Patch to render willpaginate with Twitter Bootstrap

---
+++ lib/generators/templates/app/initializers/link_renderer.rb
@@ -0,0 +1,51 @@
+module WillPaginate
+  module ViewHelpers
+    # This class does the heavy lifting of actually building the pagination
+    # links. It is used by +will_paginate+ helper internally.
+    class LinkRenderer < LinkRendererBase
+
+    protected
+    
+      def page_number(page)
+        unless page == current_page
+          link(page, page, :rel => rel_value(page), :class => "btn")
+        else
+          tag(:a, page, :class => 'current active btn')
+        end
+      end
+      
+      def gap
+        text = @template.will_paginate_translate(:page_gap) { '&hellip;' }
+        %(<a class="gap btn disabled">#{text}</a>)
+      end
+      
+      def previous_or_next_page(page, text, classname)
+        if page
+          link(text, page, :class => classname + ' btn')
+        else
+          tag(:a, text, :class => classname + ' disabled btn')
+        end
+      end
+      
+      def html_container(html)
+        html
+      end
+      
+    private
+
+      def param_name
+        @options[:param_name].to_s
+      end
+
+      def link(text, target, attributes = {})
+        if target.is_a? Fixnum
+          attributes[:rel] = rel_value(target)
+          target = url(target)
+        end
+        attributes[:href] = target
+        tag(:a, text, attributes)
+      end
+
+    end
+  end
+end
