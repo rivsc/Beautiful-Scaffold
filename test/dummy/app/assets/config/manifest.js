23c54f20298c773a83ff5da4618d5530e43f5ff9
scl <sylvain.claudel@4murs.fr>
2020-08-18 19:07:40 +0200

    Dummy ready

---
+++ test/dummy/app/assets/config/manifest.js
@@ -0,0 +1,3 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link beautiful_scaffold_manifest.js
---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  #mount Trucengine::Engine => "/beautiful_scaffold"
+end
