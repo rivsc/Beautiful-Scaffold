93c8b5c216a2113d705eb777aa3de38733a3eee0
scl <claudel.sylvain@gmail.com>
2021-02-18 20:59:35 +0100

    Caption for user model.

--- lib/generators/templates/app/models/concerns/caption_concern.rb
+++ lib/generators/templates/app/models/concerns/caption_concern.rb
@@ -3,6 +3,6 @@ module CaptionConcern
 
   # You can OVERRIDE this method used in model form and search form (in belongs_to relation)
   def caption
-    (self["name"] || self["label"] || self["description"] || "##{id}")
+    (self["name"] || self["label"] || self["description"] || self["email"] || "##{id}")
   end
 end
